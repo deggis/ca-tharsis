889baf2dcfa8673180752c10207f375ec2488707
Heikki Salo <heikki.ao.salo@iki.fi>
2024-09-03 15:07:56 +0300

    bugfix: next_link

--- main.py
+++ main.py
@@ -190,7 +190,9 @@ def _run_graph_user_query(args, result_path, initial_url):
       with open(temp_file) as in_f:
         result = json.load(in_f)
         print('Allright: Cmd: %s' % cmd)
-        next_link = result.get('@odata.nextLink').replace('$', '\\$')  # TODO: get rid of shell
+        next_link = result.get('@odata.nextLink')
+        if next_link:
+          next_link = next_link.replace('$', '\\$')  # TODO: get rid of shell
         for user in result['value']:
           all_users.append(user)
 
